7a1d02c1953871d41d52b002606bf9bb447ae37d
Anwesh43 <anweshthecool0@gmail.com>
2021-04-01 02:33:07 +0530

    adding view in activity

--- app/src/main/java/com/example/linkedmidbararcview/MainActivity.kt
+++ app/src/main/java/com/example/linkedmidbararcview/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.linkedmidbararcview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.example.midbararcview.MidBarArcView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        MidBarArcView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
