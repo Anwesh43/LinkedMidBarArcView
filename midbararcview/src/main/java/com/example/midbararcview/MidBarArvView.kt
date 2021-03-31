9514b74abddedb1898abfe15fd757d8cc446db24
Anwesh43 <anweshthecool0@gmail.com>
2021-04-01 01:48:09 +0530

    adding constants and libraries

--- midbararcview/src/main/java/com/example/midbararcview/MidBarArvView.kt
+++ midbararcview/src/main/java/com/example/midbararcview/MidBarArvView.kt
@@ -1,2 +1,26 @@
 package com.example.midbararcview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.RectF
+import android.content.Context
+import android.app.Activity
+
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#9C27B0",
+    "#004D40",
+    "#2962FF",
+    "#00C853"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val barSizeFactor : Float = 9.8f
+val rFactor : Float = 4.9f
+val delay : Long = 20
+val backColor : Int = Color.parseColor("#BDBDBD")
