598668253f42ba819b6ecc294b1984af9111671c
Anwesh43 <anweshthecool0@gmail.com>
2021-04-01 02:02:05 +0530

    adding MidBarArcView class

--- midbararcview/src/main/java/com/example/midbararcview/MidBarArvView.kt
+++ midbararcview/src/main/java/com/example/midbararcview/MidBarArvView.kt
@@ -71,3 +71,19 @@ fun Canvas.drawMBANode(i : Int, scale : Float, paint : Paint) {
     paint.color = colors[i]
     drawMidBarArc(scale, w, h, paint)
 }
+
+class MidBarArcView(ctx : Context) : View(ctx) {
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
